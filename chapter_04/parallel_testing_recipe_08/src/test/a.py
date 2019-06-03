ebb7dfe06b362ea72a324a2f700f866f19431fc7
Bjorn Lindi <blindij@users.noreply.github.com>
2019-06-03 13:07:42 +0200

    Run tests in a parallel
    This is recipe 8 from chapter 4 of the CMake Cookbook

---
+++ chapter_04/parallel_testing_recipe_08/src/test/a.py
@@ -0,0 +1,8 @@
+import sys
+import time
+
+# wait for 2 seconds
+time.sleep(0.5)
+
+# report success
+sys.exit(0)
---
+++ chapter_04/parallel_testing_recipe_08/src/test/e.py
@@ -0,0 +1,8 @@
+import sys
+import time
+
+# wait for 2 seconds
+time.sleep(1.5)
+
+# report success
+sys.exit(0)
