7868daeeb7a2c8548837e6b93251dbec3d5ee286
Bjorn Lindi <blindij@users.noreply.github.com>
2019-07-31 16:38:07 +0200

    Generate source at build time using Python
    The example is from page 240 in the CMake Cookbook. It show how to
    generate source code with Python during build time.

---
+++ chapter_06/generate_src_build_time_03/example.cpp
@@ -0,0 +1,11 @@
+#include "primes.hpp"
+#include <iostream>
+#include <vector>
+
+int main() {
+   std::cout << "all prime numbers up to " << max_number << ":";
+
+   for (auto prime : primes()) std::cout << " " << prime;
+   std::cout << std::endl;
+   return 0;
+}
